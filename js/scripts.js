3cd9b9ec2ab307e41ad5a03c173d6095f7cd8fed
Johan <ti.johan@ti7.com.br>
2022-11-25 14:35:08 -0300

    1 commit

--- js/scripts.js
+++ js/scripts.js
@@ -39,68 +39,67 @@ const AreaRetangulo = function (Base, Altura) {
     }
   } while (Verifier === true);
 };
-const AreaQuadrado = function (Lado) { 
-  let Verifier = true
+const AreaQuadrado = function (Lado) {
+  let Verifier = true;
   do {
-    Lado = parseFloat(prompt(`Informe o lado do quadrado`))
+    Lado = parseFloat(prompt(`Informe o lado do quadrado`));
 
     if (isNaN(Lado)) {
-      alert(`Digite somente números`)
+      alert(`Digite somente números`);
+    } else {
+      let Soma = Lado ** 2;
+      alert(`O quadrado de lado: ${Lado}, tem área de: ${Soma.toFixed(2)}`);
+      return (Verifier = false);
     }
-    else {
-      let Soma = Lado ** 2
-      alert(`O quadrado de lado: ${Lado}, tem área de: ${Soma.toFixed(2)}`)
-      return Verifier = false;
-    }
-  }while(Verifier === true)
-}
+  } while (Verifier === true);
+};
 const AreaCirculo = function (Raio) {
-  let Verifier = true
+  let Verifier = true;
   do {
-    Raio = parseFloat(prompt(`Informe o Raio do círculo`))
-  
-    if (isNaN(Raio)) {
-      alert(`Digite somente números`)
-    }
-    else {
-      let Soma = 3.14 * Raio ** 2
-      alert(`O círculo de raio: ${Raio}, tem área de: ${Soma.toFixed(2)}`)
-      return Verifier = false;
-    }
-  } while (Verifier === true)
-}
-const AreaTrapezio = function (BaseMaior, BaseMenor, Altura) {
-  let Verifier = true
-  do {
-    BaseMaior = parseFloat(prompt(`Informe a Base maior do trapézio`))
-    BaseMenor = parseFloat(prompt(`Informe a Base menor do trapézio`))
-    Altura = parseFloat(prompt(`Informe a Altura do trapézio`))
-  
-    if (isNaN(BaseMaior) || isNaN(BaseMenor) || isNaN(Altura)) {
-      alert(`Digite somente números`)
-    }
-    else {
-      let Soma = (BaseMaior + BaseMenor) * Altura / 2
-      alert(`O trapézio de base maior: ${BaseMaior}, base menor: ${BaseMenor}, e altura: ${Altura}, tem uma área de: ${Soma.toFixed(2)}`)
-      return Verifier = false;
-    }
-  }while(Verifier === true)
+    Raio = parseFloat(prompt(`Informe o Raio do círculo`));
 
-}
-const AreaLosango = function (DiagonalMaior, DiagonalMenor) {
-  let Verifier = true
+    if (isNaN(Raio)) {
+      alert(`Digite somente números`);
+    } else {
+      let Soma = 3.14 * Raio ** 2;
+      alert(`O círculo de raio: ${Raio}, tem área de: ${Soma.toFixed(2)}`);
+      return (Verifier = false);
+    }
+  } while (Verifier === true);
+};
+const AreaTrapezio = function (BaseMaior, BaseMenor, Altura) {
+  let Verifier = true;
   do {
-    DiagonalMaior = parseFloat(prompt(`Informe a Diagonal maior do losango`))
-    DiagonalMenor = parseFloat(prompt(`Informe a Diagonal menor do losango`))
+    BaseMaior = parseFloat(prompt(`Informe a Base maior do trapézio`));
+    BaseMenor = parseFloat(prompt(`Informe a Base menor do trapézio`));
+    Altura = parseFloat(prompt(`Informe a Altura do trapézio`));
+
+    if (isNaN(BaseMaior) || isNaN(BaseMenor) || isNaN(Altura)) {
+      alert(`Digite somente números`);
+    } else {
+      let Soma = ((BaseMaior + BaseMenor) * Altura) / 2;
+      alert(
+        `O trapézio de base maior: ${BaseMaior}, base menor: ${BaseMenor}, e altura: ${Altura}, tem uma área de: ${Soma.toFixed(
+          2
+        )}`
+      );
+      return (Verifier = false);
+    }
+  } while (Verifier === true);
+};
+const AreaLosango = function (DiagonalMaior, DiagonalMenor) {
+  let Verifier = true;
+  do {
+    DiagonalMaior = parseFloat(prompt(`Informe a Diagonal maior do losango`));
+    DiagonalMenor = parseFloat(prompt(`Informe a Diagonal menor do losango`));
 
     if (isNaN(DiagonalMaior) || isNaN(DiagonalMenor)) {
-      alert(`Digite somente números`)
+      alert(`Digite somente números`);
+    } else {
+      let Soma = (DiagonalMenor * DiagonalMaior) / 2;
     }
-    else {
-      let Soma = DiagonalMenor * DiagonalMaior / 2
-    }
-  }while(Verifier === true)
-}
+  } while (Verifier === true);
+};
 do {
   ChosenOption = parseInt(
     prompt(`Escolha uma opção abaixo
@@ -122,25 +121,24 @@ do {
       break;
     case 2:
       AreaRetangulo();
-        break;
+      break;
     case 3:
-      AreaQuadrado()
+      AreaQuadrado();
       break;
-    case 4:  
-    AreaCirculo()
+    case 4:
+      AreaCirculo();
       break;
-      case 5:
-        AreaTrapezio()
+    case 5:
+      AreaTrapezio();
       break;
-      case 6:
-        AreaTrapezio()
+    case 6:
+      AreaTrapezio();
       break;
     case 7:
       alert(`Encerando...`);
-      break
+      break;
     default:
-      alert(`Um erro ocorreu :(`)
+      alert(`Um erro ocorreu :(`);
       break;
   }
-
 } while (ChosenOption !== 7);
