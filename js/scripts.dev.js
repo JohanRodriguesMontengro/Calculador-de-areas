7226ddc352031436ab0771b314f2779999b332de
Johan <ti.johan@ti7.com.br>
2022-11-25 14:29:04 -0300

    start day

--- js/scripts.dev.js
+++ js/scripts.dev.js
@@ -42,12 +42,61 @@ var AreaQuadrado = function AreaQuadrado(Lado) {
   do {
     Lado = parseFloat(prompt("Informe o lado do quadrado"));
 
-    if (isNaN(Altura)) {
+    if (isNaN(Lado)) {
       alert("Digite somente n\xFAmeros");
     } else {
       var Soma = Math.pow(Lado, 2);
-      alert("O quadrado de lado: ".concat(Lado, ", tem altura de: ").concat(Soma.toFixed(2)));
+      alert("O quadrado de lado: ".concat(Lado, ", tem \xE1rea de: ").concat(Soma.toFixed(2)));
       return Verifier = false;
+    }
+  } while (Verifier === true);
+};
+
+var AreaCirculo = function AreaCirculo(Raio) {
+  var Verifier = true;
+
+  do {
+    Raio = parseFloat(prompt("Informe o Raio do c\xEDrculo"));
+
+    if (isNaN(Raio)) {
+      alert("Digite somente n\xFAmeros");
+    } else {
+      var Soma = 3.14 * Math.pow(Raio, 2);
+      alert("O c\xEDrculo de raio: ".concat(Raio, ", tem \xE1rea de: ").concat(Soma.toFixed(2)));
+      return Verifier = false;
+    }
+  } while (Verifier === true);
+};
+
+var AreaTrapezio = function AreaTrapezio(BaseMaior, BaseMenor, Altura) {
+  var Verifier = true;
+
+  do {
+    BaseMaior = parseFloat(prompt("Informe a Base maior do trap\xE9zio"));
+    BaseMenor = parseFloat(prompt("Informe a Base menor do trap\xE9zio"));
+    Altura = parseFloat(prompt("Informe a Altura do trap\xE9zio"));
+
+    if (isNaN(BaseMaior) || isNaN(BaseMenor) || isNaN(Altura)) {
+      alert("Digite somente n\xFAmeros");
+    } else {
+      var Soma = (BaseMaior + BaseMenor) * Altura / 2;
+      alert("O trap\xE9zio de base maior: ".concat(BaseMaior, ", base menor: ").concat(BaseMenor, ", e altura: ").concat(Altura, ", tem uma \xE1rea de: ").concat(Soma.toFixed(2)));
+      return Verifier = false;
+    }
+  } while (Verifier === true);
+};
+
+var AreaLosango = function AreaLosango(DiagonalMaior, DiagonalMenor) {
+  var Verifier = true;
+
+  do {
+    DiagonalMaior = parseFloat(prompt("Informe a Diagonal maior do losango"));
+    DiagonalMenor = parseFloat(prompt("Informe a Diagonal menor do losango"));
+
+    if (isNaN(DiagonalMaior) || isNaN(DiagonalMenor)) {
+      alert("Digite somente n\xFAmeros");
+    } else {
+      var Soma = DiagonalMenor * DiagonalMaior / 2;
     }
   } while (Verifier === true);
 };
@@ -55,21 +104,41 @@ var AreaQuadrado = function AreaQuadrado(Lado) {
 do {
   ChosenOption = parseInt(prompt("Escolha uma op\xE7\xE3o abaixo\n  1- Calcular a \xE1rea do tri\xE2ngulo\n  2- Calcular a \xE1rea do ret\xE2ngulo\n  3- Calcular a \xE1rea do quadrado\n  4- Calcular a \xE1rea do c\xEDrculo\n  5- Calcular a \xE1rea do trap\xE9zio\n  6- Calcular a \xE1rea do losango\n  7- Sair"));
 
-  if (isNaN(ChosenOption) || ChosenOption >= 8 || ChosenOption <= 0) {
-    alert("Digite somente n\xFAmeros entre 1 a 7");
-  } else if (ChosenOption === 1) {
-    AreaTriangulo();
-  } else if (ChosenOption === 2) {
-    AreaRetangulo();
-  } else if (ChosenOption === 3) {
-    AreaQuadrado();
-  } else if (ChosenOption === 4) {
-    AreaCirculo();
-  } else if (ChosenOption === 5) {
-    AreaTrapezio();
-  } else if (ChosenOption === 6) {
-    AreaLosango();
-  } else if (ChosenOption === 7) {
-    alert("Encerando...");
+  switch (ChosenOption) {
+    case isNaN(ChosenOption) || ChosenOption >= 8 || ChosenOption <= 0:
+      alert("Digite somente n\xFAmeros entre 1 a 7");
+      break;
+
+    case 1:
+      AreaTriangulo();
+      break;
+
+    case 2:
+      AreaRetangulo();
+      break;
+
+    case 3:
+      AreaQuadrado();
+      break;
+
+    case 4:
+      AreaCirculo();
+      break;
+
+    case 5:
+      AreaTrapezio();
+      break;
+
+    case 6:
+      AreaTrapezio();
+      break;
+
+    case 7:
+      alert("Encerando...");
+      break;
+
+    default:
+      alert("Um erro ocorreu :(");
+      break;
   }
 } while (ChosenOption !== 7);
--- js/scripts.js
+++ js/scripts.js
@@ -44,16 +44,63 @@ const AreaQuadrado = function (Lado) {
   do {
     Lado = parseFloat(prompt(`Informe o lado do quadrado`))
 
-    if (isNaN(Altura)) {
+    if (isNaN(Lado)) {
       alert(`Digite somente números`)
     }
     else {
       let Soma = Lado ** 2
-      alert(`O quadrado de lado: ${Lado}, tem altura de: ${Soma.toFixed(2)}`)
+      alert(`O quadrado de lado: ${Lado}, tem área de: ${Soma.toFixed(2)}`)
       return Verifier = false;
     }
   }while(Verifier === true)
- }
+}
+const AreaCirculo = function (Raio) {
+  let Verifier = true
+  do {
+    Raio = parseFloat(prompt(`Informe o Raio do círculo`))
+  
+    if (isNaN(Raio)) {
+      alert(`Digite somente números`)
+    }
+    else {
+      let Soma = 3.14 * Raio ** 2
+      alert(`O círculo de raio: ${Raio}, tem área de: ${Soma.toFixed(2)}`)
+      return Verifier = false;
+    }
+  } while (Verifier === true)
+}
+const AreaTrapezio = function (BaseMaior, BaseMenor, Altura) {
+  let Verifier = true
+  do {
+    BaseMaior = parseFloat(prompt(`Informe a Base maior do trapézio`))
+    BaseMenor = parseFloat(prompt(`Informe a Base menor do trapézio`))
+    Altura = parseFloat(prompt(`Informe a Altura do trapézio`))
+  
+    if (isNaN(BaseMaior) || isNaN(BaseMenor) || isNaN(Altura)) {
+      alert(`Digite somente números`)
+    }
+    else {
+      let Soma = (BaseMaior + BaseMenor) * Altura / 2
+      alert(`O trapézio de base maior: ${BaseMaior}, base menor: ${BaseMenor}, e altura: ${Altura}, tem uma área de: ${Soma.toFixed(2)}`)
+      return Verifier = false;
+    }
+  }while(Verifier === true)
+
+}
+const AreaLosango = function (DiagonalMaior, DiagonalMenor) {
+  let Verifier = true
+  do {
+    DiagonalMaior = parseFloat(prompt(`Informe a Diagonal maior do losango`))
+    DiagonalMenor = parseFloat(prompt(`Informe a Diagonal menor do losango`))
+
+    if (isNaN(DiagonalMaior) || isNaN(DiagonalMenor)) {
+      alert(`Digite somente números`)
+    }
+    else {
+      let Soma = DiagonalMenor * DiagonalMaior / 2
+    }
+  }while(Verifier === true)
+}
 do {
   ChosenOption = parseInt(
     prompt(`Escolha uma opção abaixo
@@ -66,21 +113,34 @@ do {
   7- Sair`)
   );
 
-  if (isNaN(ChosenOption) || ChosenOption >= 8 || ChosenOption <= 0) {
-    alert(`Digite somente números entre 1 a 7`);
-  } else if (ChosenOption === 1) {
-    AreaTriangulo();
-  } else if (ChosenOption === 2) {
-    AreaRetangulo();
-  } else if (ChosenOption === 3) {
-    AreaQuadrado()
-  } else if (ChosenOption === 4) {
+  switch (ChosenOption) {
+    case isNaN(ChosenOption) || ChosenOption >= 8 || ChosenOption <= 0:
+      alert(`Digite somente números entre 1 a 7`);
+      break;
+    case 1:
+      AreaTriangulo();
+      break;
+    case 2:
+      AreaRetangulo();
+        break;
+    case 3:
+      AreaQuadrado()
+      break;
+    case 4:  
     AreaCirculo()
-  } else if (ChosenOption === 5) {
-    AreaTrapezio()
-  } else if (ChosenOption === 6) {
-    AreaLosango()
-  } else if (ChosenOption === 7) {
-    alert(`Encerando...`);
+      break;
+      case 5:
+        AreaTrapezio()
+      break;
+      case 6:
+        AreaTrapezio()
+      break;
+    case 7:
+      alert(`Encerando...`);
+      break
+    default:
+      alert(`Um erro ocorreu :(`)
+      break;
   }
+
 } while (ChosenOption !== 7);
